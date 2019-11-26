c023b57969afa677a29f7f171e366904a70e2c3a
Brian Turner <brianturner@framepush.com>
2019-11-26 16:15:52 -0500

    Documentation

--- IBackStackable.cs
+++ IBackStackable.cs
@@ -2,7 +2,14 @@ namespace FramePush.BackStack
 {
     public interface IBackStackable
     {
+        /// <summary>
+        /// Called when back or escape pressed. Should perform cleanup.
+        /// </summary>
         void Dismiss();
+        
+        /// <summary>
+        /// Re-initialize when coming back from another item.
+        /// </summary>
         void GoBackTo();
     }
 }
